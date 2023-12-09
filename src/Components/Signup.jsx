2f62ce551fa5acbf9115008e368dedd23cda8fb8
samer <rouphaels@gmail.com>
2023-12-09 15:09:52 +0200

    login-signup

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,6 +1,6 @@
 import '../Style/Signup.css';
 import signup from '../images/signup .jpg';
-import close from '../images/close.svg'
+import back from '../images/back.svg'
 import user from '../images/user.svg'
 import lastname from '../images/lastname.svg'
 import password from '../images/password.svg'
@@ -15,90 +15,94 @@ const Signup = () => {
         <div className='signup'>
 
             <div className="signup-div">
-                <img src={signup} className="signup-image" />
-                <p>
-                    {" "}
-                    We always make our customer
-                    <br />happy by providing as many choises
-                    <br />as possible.
-                </p>
-            </div>
-
-            <div className="part1">
-                <div className="part2">
-                    <div className="close">
-                        <Link to="/"><img src={close} className="close-img" /></Link>
-                    </div>
-                    <p className="signup-title">
-                        Sign Up
-                    </p>
-
-                    <div className="user1">
-
-                        <img src={user} className="login-img" />
-
-                        <div className=" text">
-                            <input type="text" id="username" placeholder="username" className="login-input" />
-                        </div>
-                    </div>
-                    <div className="user1">
-
-                        <img src={lastname} className="login-img" />
-
-                        <div className=" text">
-                            <input type="text" id="username" placeholder="Lastname" className="login-input" />
-                        </div>
-                    </div>
-                    <div className="user1">
-
-                        <img src={email} className="login-img" />
-
-                        <div className=" text">
-                            <input type="text" id="username" placeholder="Email" className="login-input" />
-                        </div>
-                    </div>
-                    <div className="user1">
-
-                        <img src={password} className="login-img" />
-
-                        <div className=" text">
-                            <input type="text" id="username" placeholder="Password" className="login-input" />
-                        </div>
-                    </div>
-
-                    <div className="user1">
-
-                        <img src={confpassword} className="login-img" />
-
-                        <div className=" text">
-                            <input type="text" id="username" placeholder="Confirm Password" className="login-input" />
-                        </div>
-                    </div>
-                    <div className="user1">
-
-                        <img src={phone} className="login-img" />
-
-                        <div className=" text">
-                            <input type="text" id="username" placeholder="Phone" className="login-input" />
-                        </div>
-                    </div>
-                    <div className="user1">
-
-                        <img src={address} className="login-img" />
-
-                        <div className=" text">
-                            <input type="text" id="username" placeholder="Adderss" className="login-input" />
-                        </div>
-                    </div>
-                    <button type="button" className="loginbt">
-                        Create Account
-                    </button>
-                    <p> Already have an account? <Link to="/Login">Sign in </Link></p>
-
+                
+                    <div className="back">
+                        <Link to="/"><img src={back} className="back-img" /></Link>
+                   
                 </div>
-
-
+                <div className="signup-div1">
+                    <img src={signup} className="signup-image" />
+                    <p>
+                        {" "}
+                        We always make our customer
+                        <br />happy by providing as many choises as possible.
+                    </p>
+                </div>
             </div>
+            <div className='signup-div2'>
+            <div className='part'>
+                <div className="part1">
+                    <div className="part2">
+                        <p className="signup-title">
+                            Sign Up
+                        </p>
+                        <div className="user1">
+
+                            <img src={user} className="login-img" />
+
+                            <div className=" text">
+                                <input type="text" id="username" placeholder="username" className="login-input" />
+                            </div>
+                        </div>
+                        <div className="user1">
+
+                            <img src={lastname} className="login-img" />
+
+                            <div className=" text">
+                                <input type="text" id="username" placeholder="Lastname" className="login-input" />
+                            </div>
+                        </div>
+                        <div className="user1">
+
+                            <img src={email} className="login-img" />
+
+                            <div className=" text">
+                                <input type="text" id="username" placeholder="Email" className="login-input" />
+                            </div>
+                        </div>
+                        <div className="user1">
+
+                            <img src={password} className="login-img" />
+
+                            <div className=" text">
+                                <input type="text" id="username" placeholder="Password" className="login-input" />
+                            </div>
+                        </div>
+
+                        <div className="user1">
+
+                            <img src={confpassword} className="login-img" />
+
+                            <div className=" text">
+                                <input type="text" id="username" placeholder="Confirm Password" className="login-input" />
+                            </div>
+                        </div>
+                        <div className="user1">
+
+                            <img src={phone} className="login-img" />
+
+                            <div className=" text">
+                                <input type="text" id="username" placeholder="Phone" className="login-input" />
+                            </div>
+                        </div>
+                        <div className="user1">
+
+                            <img src={address} className="login-img" />
+
+                            <div className=" text">
+                                <input type="text" id="username" placeholder="Adderss" className="login-input" />
+                            </div>
+                        </div>
+                        <div className='btn-signup'>
+                            <button type="button" className="submitbt">
+                                Create Account
+                            </button>
+                            <p> Already have an account? <Link to="/Login">Sign in </Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     )
 };
